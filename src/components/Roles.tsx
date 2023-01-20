c98ba59af02d1a340153d09e950eec9f40ed207a
Andrew Dutson <Dutsandrew@gmail.com>
2023-01-20 11:29:21 -0700

    styling for roles animation

--- src/components/Roles.tsx
+++ src/components/Roles.tsx
@@ -11,26 +11,30 @@ const Roles = () => {
 
   useEffect(() => {
     let currentRoleIndex = 1;
-    const roleText = document.querySelector('.role-text');
+    const roleText = document.querySelector('#current-role');
 
     if (roleText) {
       roleText.textContent = currentRoles[0];
 
       setInterval(() => {
+        roleText.classList.remove('new-role');
+        void (roleText as any).offsetWidth;
         roleText.textContent = currentRoles[currentRoleIndex];
+        roleText.classList.add('new-role');
         currentRoleIndex++;
 
         if (currentRoleIndex > currentRoles.length - 1) {
           currentRoleIndex = 0;
         };
-      }, 2500);
+      }, 3500);
     };
   }, []);
 
   return (
     <section 
       className="roles-container">
-      <p 
+      <p
+        id="current-role"
         className="role-text">
       </p>
     </section>
